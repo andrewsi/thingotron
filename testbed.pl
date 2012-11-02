d0c1b93830fed02562af2f2dccd578b1c15731da
Andrew <Andrew.Sims@matrixit.net>
2012-11-02 17:07:04 -0400

    Configuration file and testbed for sample twitter code

--- testbed.pl
+++ testbed.pl
@@ -8,10 +8,6 @@ my $config = new LoadConfig("C:\\Perl\\programs\\thingotron\\twitter.config");
 
 my $twitter = new Twitter($config);
 
-my $values = $twitter->getUserDetails($config->{"user"});
-
-foreach my $entry (keys(%{$values})) {
-	print $entry . "\t" . $values->{$entry} . "\n";
-}
+$twitter->tweet("Hello, world");
 
 exit;
